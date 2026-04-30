fb5af32668bce37afc1fb4c277d153c7a4b4cfa2
SotirUsama1 <sotirusama16@gmail.com>
2026-05-01 01:46:07 +0300

    Test world syncronization

---
+++ GazeboSim/runGazebo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker run -it \
+    --gpus all \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="XAUTHORITY=/root/.Xauthority" \
+    --env="__GLX_VENDOR_LIBRARY_NAME=nvidia" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
+    --volume="$(pwd)/worlds:/root/worlds:rw" \
+    gazebo:latest \
+    gazebo "$@"
